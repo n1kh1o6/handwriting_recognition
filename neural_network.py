fc477a5b617be2a99e9497d042fec0c835bf01e4
n1kh1lo6 <nickprab59@gmail.com>
2025-06-29 23:07:00 +0530

    did bug fixes and added feedforward function to evalualte accuracy of my model

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+# helper functions
 def sigma(z):
     return 1.0/(1+np.exp(-z))
 
@@ -17,14 +18,8 @@ class Neural_Network:
         self.weights=[]
         self.biases=[]
         for i in range (1,self.layers):
-            self.weights.append(np.random.randn((nodes[i],nodes[i-1])))# array of shape y,x
-            self.biases.append(np.random.randn((nodes[i],1)))# array of shape y,1
-
-    # #function to return output
-    # def feedforward(self,a):
-    #     for w,b in zip(self.weights,self.biases):
-    #         a=w@a+b
-    #     return a
+            self.weights.append(np.random.randn(nodes[i],nodes[i-1]))# array of shape y,x
+            self.biases.append(np.random.randn(nodes[i],1))# array of shape y,1
     
     #function for the formation of initial mini batches
     def SGD(self,training_data,epochs,mini_batch_size,lr):
@@ -42,41 +37,51 @@ class Neural_Network:
         dw=[] #each time 
         m=len(mini_batch)
         for w,b in zip(self.weights,self.biases):
-            db.append(np.zeros(b.shape()))
-            dw.append(np.zeros(w.shape()))
+            db.append(np.zeros(b.shape))
+            dw.append(np.zeros(w.shape))
         for x,y in mini_batch:
             delta_db,delta_dw=self.backprop(x,y)
             db=[db+ddb for db,ddb in zip(db,delta_db)]
-            db=[dw+ddw for ddw,ddw in zip(dw,delta_dw)]
-        w-=learning_rate/m*dw
-        b-=learning_rate/w*db
+            dw=[dw+ddw for dw,ddw in zip(dw,delta_dw)]
+        self.weights = [w-(learning_rate/m)*nw
+                        for w, nw in zip(self.weights, dw)]
+        self.biases = [b-(learning_rate/m)*nb
+                       for b, nb in zip(self.biases, db)]
 
     #to calculate delc/delw and delc/delb (gradients of the cost function for a given x) using backpropogation formulae
     def backprop(self,x,y):
         db=[] #here db and dw and the actual cost derivatives which will be calculated 
         dw=[] 
         for w,b in zip(self.weights,self.biases):
-            db.append(np.zeros(b.shape()))
-            dw.append(np.zeros(w.shape()))
+            db.append(np.zeros(b.shape))
+            dw.append(np.zeros(w.shape))
         activation=x
         activations=[x]
         zs=[]
-        for w,b in zip(self.weghts,self.biases):
+        for w,b in zip(self.weights,self.biases):
             z=np.dot(w,x)+b
             zs.append(z)
             activation=sigma(z)
             activations.append(activation)
-        error=np.dot(self.cost_derivative(y,activations[-1]),sigma_prime(z[-1]))
+        error=self.cost_derivative(y,activations[-1])*sigma_prime(zs[-1])
         db[-1]=error 
-        dw[-1]=error@(activations[-2].transpose())
+        dw[-1]=error@(activations[-2].T)
         for i in range(2,self.layers):
-            error=np.dot(((self.weights[-i+1].transpose())@error),sigma_prime[zs[-i]])
+            error=((self.weights[-i+1].T)@error)*sigma_prime(zs[-i])
             db[-i]=error 
-            dw[-i]=error@(activations[-i-1].transpose())
+            dw[-i]=error@(activations[-i-1].T)
         return (db,dw)
 
+    # function to return C'
     def cost_derivative(self,y,a):
         return a-y
+    
+    #to evaluate output based on our model for the network
+    def feedforward(self, a):
+        for w, b in zip(self.weights, self.biases):
+            a = sigma(np.dot(w, a) + b)
+        return a
+
             
 
         
